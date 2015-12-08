d991fa6570151161183256c47df2f1511c721ea4
Zach Gannon <zachgannon93@gmail.com>
2015-12-07 19:07:34 -0500

    Fixed PyPI download URI.

--- vycro/__about__.py
+++ vycro/__about__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, print_function, division
 __version__ = '1.0.0'
 __title__ = 'vycro'
 __summary__ = 'VBA macro wrapper for Python to be used with Microsoft Excel.'
-__uri__ = 'https://github.com/gannon93/python/vycro'
+__uri__ = 'https://github.com/gannon93/vycro'
 
 __author__ = 'Zach Gannon'
 __email__ = 'zachgannon93@gmail.com'
